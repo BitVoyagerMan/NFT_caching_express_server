6062769490cfa0b03dd14b04f277f996ddaf551d
MINATO-LEGEND <earth04190419@gmail.com>
2023-05-08 06:14:28 -0700

    finished

--- routes/router.js
+++ routes/router.js
@@ -6,5 +6,5 @@ const cacheController = require('../controllers/cacheController');
 router.get('/getAll/:address', cacheController.getNFTsbyContract);
 router.get('/getAll/:address/:userWallet', cacheController.getNFTsByContractAndUser);
 router.get('/getWalletNFTs/:userWallet', cacheController.getNFTsByUser);
-router.post('/addContract', cacheController.addContract);
+router.post('/subscribe', cacheController.addContract);
 module.exports = router;
